e08c4942f4f990bcb2fe48be72f5676aa85eb3f2
Mahmoud Emad Eldin <mahmoud.emad@areebgroup.com>
2021-03-05 14:42:39 +0200

    Added navbar items

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import './Navbar.scss'
+import logo from './../../assets/images/logo.png'
+import { Search } from '@emotion-icons/material/Search'
+import { Person } from '@emotion-icons/material/Person'
+
+const Navbar = ( props ) => {
+    const productName = "ViuLive"
+    const [ searchVal, setSearchVal ] = useState( '' )
+
+    const detectSearchValue = ( e ) => setSearchVal( e.target.value )
+
+    return (
+        <nav>
+            <img className="logo" src={ logo } alt={ productName } />
+
+            <div className="searchInput">
+                <input type="search" value={ searchVal } placeholder="Search..." onChange={ e => detectSearchValue( e ) } />
+                <Search className="searchIcon" title="Search" role="button" />
+            </div>
+
+            <div className="userDetails">
+                <Person className="userIcon" role="button" />
+                <span>welcome to { productName }</span>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
