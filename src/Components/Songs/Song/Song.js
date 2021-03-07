f4c6781b7040307afdd0c56c58a93e140bb8123b
Mahmoud Emad Eldin <mahmoud.emad@areebgroup.com>
2021-03-07 17:25:21 +0200

    Restructure song component html elements

--- src/Components/Songs/Song/Song.js
+++ src/Components/Songs/Song/Song.js
@@ -1,24 +1,31 @@
 import React from 'react'
 import Rating from '../../Rating/Rating'
 import { FavoriteBorder } from '@emotion-icons/material/FavoriteBorder'
+import { Favorite } from '@emotion-icons/material/Favorite'
 import './Song.scss'
 
 
-const Song = ({ song }) => {
+const Song = ({ song, inFavorite }) => {
     const starColor = '#5844af';
+    const favorite = () => {
+        inFavorite = !inFavorite;
+        console.log(inFavorite)
+    }
 
     return (
         <div className="song">
+            <img src={ song.images } alt={ song.title } />
             <div className="songDetails">
-                <img src={ song.images } alt={ song.title } />
                 <div className="songDetailsTitle">
                     <h3>{ song.title }</h3>
                     <p>{ song.artist }</p>
                 </div>
-            </div>
-            <div className="songRate">
-                <Rating starPoints={ song.level } starColor={ starColor } />
-                <FavoriteBorder role="button" />
+                <div className="songRate">
+                    <Rating starPoints={ song.level } starColor={ starColor } />
+                    <span onClick={ favorite }>
+                        { inFavorite ? <FavoriteBorder role="button" /> : <Favorite role="button" /> }
+                    </span>
+                </div>
             </div>
         </div>
     )
--- src/Components/Songs/Songs.js
+++ src/Components/Songs/Songs.js
@@ -1,38 +1,44 @@
-import React from 'react'
+import { useState } from 'react'
 import Song from './Song/Song'
 
 const Songs = () => {
-    const songs = [
-        {
-            "id": "5b8e47deb3984c68ed8192e3",
-            "title": "Easter Oratorio (in Am)",
-            "artist": "Johann Sebastian Bach",
-            "images": "https://d3mzlbmn9ukddk.cloudfront.net/songs/image/586a75fa-99df-4ce0-af71-55f6a474c404.jpg",
-            "level": 6,
-            "search": "johann sebastian bach easter oratorio (in am)"
-        },
-        {
-            "id": "5b8e47deb3984c68ed8192e3",
-            "title": "Easter Oratorio (in Am)",
-            "artist": "Johann Sebastian Bach",
-            "images": "https://d3mzlbmn9ukddk.cloudfront.net/songs/image/586a75fa-99df-4ce0-af71-55f6a474c404.jpg",
-            "level": 6,
-            "search": "johann sebastian bach easter oratorio (in am)"
-        },
-        {
-            "id": "5b8e47deb3984c68ed8192e3",
-            "title": "Easter Oratorio (in Am)",
-            "artist": "Johann Sebastian Bach",
-            "images": "https://d3mzlbmn9ukddk.cloudfront.net/songs/image/586a75fa-99df-4ce0-af71-55f6a474c404.jpg",
-            "level": 6,
-            "search": "johann sebastian bach easter oratorio (in am)"
-        },
-    ]
+    const [ songs, setSongs ] = useState([ {
+        "id": "5b8e47deb3984c68ed8192e3",
+        "title": "Easter Oratorio (in Am)",
+        "artist": "Johann Sebastian Bach",
+        "images": "https://d3mzlbmn9ukddk.cloudfront.net/songs/image/586a75fa-99df-4ce0-af71-55f6a474c404.jpg",
+        "level": 6,
+        "search": "johann sebastian bach easter oratorio (in am)"
+    },
+    {
+        "id": "5b8f736fb3984c13548374af",
+        "title": "Fysyoni (in Bm)",
+        "artist": "Traditional",
+        "images": "https://d3mzlbmn9ukddk.cloudfront.net/songs/image/2524751a-c107-40e5-8de8-08e68b9d8ca8.jpg",
+        "level": 10,
+        "search": "traditional fysyoni (in bm)"
+    },
+    {
+        "id": "5b8e471cb3984c68ed81926d",
+        "title": "Gettin' Along",
+        "artist": "The unknown",
+        "images": "https://d3mzlbmn9ukddk.cloudfront.net/songs/image/90c50bda-6935-49df-b5db-bbb493af4fed.jpg",
+        "level": 12,
+        "search": "the unknown gettin' along"
+    },
+    {
+        "id": "5b8e447eb3984c68ed8190bf",
+        "title": "Fysouni (in Bm)",
+        "artist": "Traditional",
+        "images": "https://d3mzlbmn9ukddk.cloudfront.net/songs/image/4c08e106-c82e-4c61-ae7c-18068023297c.jpg",
+        "level": 3,
+        "search": "traditional fysouni (in bm)"
+    } ])
 
     return (
         <div>
             {songs.map((song) => (
-                <Song song={ song } key={ song.id } />
+                <Song song={ song } key={ song.id } inFavorite="true" />
             )) }
         </div>
     )
